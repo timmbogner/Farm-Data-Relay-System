3bfe42b360ca9be9c1d18d2107ef4993ae9e6ee7
Timm Bogner <64260873+timmbogner@users.noreply.github.com>
2022-06-18 23:06:03 -0500

    added stress tester

---
+++ Examples/ESPNOW_Stress_Test/ESPNOW_Stress_Test.ino
@@ -0,0 +1,19 @@
+//  FARM DATA RELAY SYSTEM
+//
+//  ESP-NOW Stress Tester or "Spammer"
+//  
+//  Sends ESP-NOW packets at approximately 60Hz.
+//
+
+#include "fdrs_sensor.h"
+
+void setup() {
+  beginFDRS();
+}
+void loop() {
+  for (int i=0; i < 255; i++) {
+    loadFDRS(float(i), IT_T);
+    sendFDRS();
+    delay(15);
+  }
+}
