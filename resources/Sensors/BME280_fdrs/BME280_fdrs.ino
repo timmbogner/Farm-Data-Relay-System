5ef5b73df312df3198b7815027a52301796a2cca
Sascha <juch@gmx.de>
2022-07-06 00:33:32 +0200

    Cleaned up library base structure
    * keywords.txt (for syntax highlighting), library.properties (for a proper description added
    * moved Universal_Sensor_beta to the examples directory
    * moved Sensors, FDRS_Sensor and FDRS_Gateway to resources folder. They contain documentation which needs to be kept (to be discussed where to put that later on) but are fully redundant as they are just duplicates of the different sensors / gateways in the examples folder.

---
+++ resources/Sensors/BME280_fdrs/BME280_fdrs.ino
@@ -0,0 +1,28 @@
+//  FARM DATA RELAY SYSTEM
+//
+//  BME280 SENSOR MODULE
+//
+//  Developed by Timm Bogner (bogner1@gmail.com) for Sola Gratia Farm in Urbana, Illinois, USA.
+
+#include "sensor_setup.h"
+#include <Adafruit_BME280.h>
+#include <fdrs_sensor.h>
+
+Adafruit_BME280 bme;
+
+void setup() {
+  //Serial.begin(115200);
+  beginFDRS();
+  while (!bme.begin(0x76)) {
+    //Serial.println("BME not initializing!");
+    delay(10);
+  }
+}
+
+void loop() {
+  loadFDRS(bme.readTemperature(), TEMP_T);
+  loadFDRS(bme.readHumidity(), HUMIDITY_T);
+  loadFDRS(bme.readPressure() / 100.0F, PRESSURE_T);
+  sendFDRS();
+  sleepFDRS(60);  //Sleep time in seconds
+}
