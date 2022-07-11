700f87b6a4cf7557cbe7daa51a87c6c8d9c9a839
Sascha <juch@gmx.de>
2022-07-11 16:25:03 +0200

    houeskeeping in Universal Sensor's config file
    Put FDRS_DEBUG to top
    Some formatting and further explanations on the settings.
    Uncommented local LoRa settings, so by default the global fdrs settings are taken.
    This makes the setup process easier and less error-prone.

--- examples/Universal_Sensor_beta/fdrs_sensor_config.h
+++ examples/Universal_Sensor_beta/fdrs_sensor_config.h
@@ -1,29 +1,41 @@
 //  FARM DATA RELAY SYSTEM
 //
-//  Sensor Configuration
-//  (This file will soon be known as 'sensor_config.h')
+//  Sensor Configuration File
+//	By default global settings will be used for this sensor.
+//	If you need to change a specific setting, uncomment and set to your needs further below.
 //
 
-//#include <fdrs_globals.h> //Uncomment when you install the globals file
+#define FDRS_DEBUG 			// Comment, if you do not want to see debug messages
+#include <fdrs_globals.h> 	// Comment if you want to set specific values for this sensor here
 
-#define READING_ID    1   //Unique ID for this sensor
-#define GTWY_MAC      0x04 //Address of the nearest gateway
+#define READING_ID    1   	//Unique ID for this sensor
+#define GTWY_MAC      0x04 	//Address of the nearest gateway
 
 #define DEEP_SLEEP
 //#define POWER_CTRL    14
-#define FDRS_DEBUG
 
-//SPI Configuration -- Needed only on Boards with multiple SPI interfaces like the ESP32
+// TODO: New, taken from Gateway - should be configured in the same way as there!
+//Pins for UART data interface (ESP32 only)
+#define RXD2 14
+#define TXD2 15
+
+// SPI Configuration -- Needed only on Boards with multiple SPI interfaces like the ESP32
 #define SPI_SCK 5
 #define SPI_MISO 19
 #define SPI_MOSI 27
 
-//LoRa Configuration
+// LoRa Pin Configuration -- Uncomment and configure if you are using LoRa
 #define LORA_SS 18
 #define LORA_RST 14
 #define LORA_DIO0 26
+
+
+// LoRa Transport Configuration -- This should be set in the global configuration file. However, if you need a
+// specific band and spreading factor for this sensor, configure below.
 //433E6 for Asia
 //866E6 for Europe
 //915E6 for North America
-#define LORA_BAND 915E6
-#define LORA_SF 7
+//#define LORA_BAND 915E6
+//#define LORA_SF 7
+
+// TODO: Addd local MQTT configuration
