91aeb11402a7408d2122181b6410d934c91d3d31
Timm Bogner <64260873+timmbogner@users.noreply.github.com>
2022-10-06 05:33:35 -0500

    remove .gitignore

---
+++ examples/Sensor_Examples/SHT20_fdrs/SHT20_fdrs.ino
@@ -0,0 +1,24 @@
+//  FARM DATA RELAY SYSTEM
+//
+//  SENSIRION SHT20 SENSOR MODULE
+//
+//  Developed by Timm Bogner (timmbogner@gmail.com) in Urbana, Illinois, USA.
+//  If you are using the sensor for air monitoring, change SOIL_T to HUMIDITY_T.
+//
+#include "DFRobot_SHT20.h"
+#include "fdrs_sensor_config.h"
+#include <fdrs_node.h>
+
+DFRobot_SHT20 sht20(&Wire, SHT20_I2C_ADDR);
+
+
+void setup() {
+  beginFDRS();
+   sht20.initSHT20();
+}
+void loop() {
+  loadFDRS(sht20.readHumidity(), SOIL_T);
+  loadFDRS(sht20.readTemperature(), TEMP_T);
+  sendFDRS();
+  sleepFDRS(300);  //Sleep time in seconds
+}
