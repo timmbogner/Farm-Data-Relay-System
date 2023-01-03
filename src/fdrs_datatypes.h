8638c2730b7b85caea16c933e136cb89043ac75a
Stips5 <smedak95@gmail.com>
2023-01-03 22:16:12 +0100

    PM data types #126

--- src/fdrs_datatypes.h
+++ src/fdrs_datatypes.h
@@ -11,25 +11,29 @@
 #define HUMIDITY_T      3  // Relative Humidity 
 #define PRESSURE_T      4  // Atmospheric Pressure 
 #define LIGHT_T         5  // Light (lux) 
-#define SOIL_T          6  // Soil Moisture 
-#define SOIL2_T         7  // Soil Moisture #2 
-#define SOILR_T         8 // Soil Resistance 
-#define SOILR2_T        9 // Soil Resistance #2 
-#define OXYGEN_T        10 // Oxygen 
-#define CO2_T           11 // Carbon Dioxide
-#define WINDSPD_T       12 // Wind Speed
-#define WINDHDG_T       13 // Wind Direction
-#define RAINFALL_T      14 // Rainfall
-#define MOTION_T        15 // Motion
-#define VOLTAGE_T       16 // Voltage
-#define VOLTAGE2_T      17 // Voltage #2
-#define CURRENT_T       18 // Current
-#define CURRENT2_T      19 // Current #2
-#define IT_T            20 // Iterations
-#define LATITUDE_T      21 // GPS Latitude
-#define LONGITUDE_T     22 // GPS Longitude
-#define ALTITUDE_T      23 // GPS Altitude
-#define HDOP_T          24 // GPS HDOP
-#define LEVEL_T         25 // Fluid Level
+#define UV_T			6  // UV
+#define SOIL_T          7  // Soil Moisture 
+#define SOIL2_T         8  // Soil Moisture #2 
+#define SOILR_T         9 // Soil Resistance 
+#define SOILR2_T        10 // Soil Resistance #2 
+#define OXYGEN_T        11 // Oxygen 
+#define CO2_T           12 // Carbon Dioxide
+#define WINDSPD_T       13 // Wind Speed
+#define WINDHDG_T       14 // Wind Direction
+#define RAINFALL_T      15 // Rainfall
+#define MOTION_T        16 // Motion
+#define VOLTAGE_T       17 // Voltage
+#define VOLTAGE2_T      18 // Voltage #2
+#define CURRENT_T       19 // Current
+#define CURRENT2_T      20 // Current #2
+#define IT_T            21 // Iterations
+#define LATITUDE_T      22 // GPS Latitude
+#define LONGITUDE_T     23 // GPS Longitude
+#define ALTITUDE_T      24 // GPS Altitude
+#define HDOP_T          25 // GPS HDOP
+#define LEVEL_T         26 // Fluid Level
+#define PM1_T	        27 // 1 Particles
+#define PM2_5_T         28 // 2.5 Particles
+#define PM10_T	        29 // 10 Particles
 
 #endif //FDRS_DATA_TYPES
