5f2fd0e586cead3e7dbf1c73e59a63b0bc9a1db0
Timm Bogner <64260873+timmbogner@users.noreply.github.com>
2023-02-22 08:17:38 -0600

    add LoRa controller example

---
+++ examples/LoRa_Controller/LoRa_Controller.ino
@@ -0,0 +1,26 @@
+//  FARM DATA RELAY SYSTEM
+//
+//  ESP-NOW Sensor Example
+//
+//  Developed by Timm Bogner (timmbogner@gmail.com) for Sola Gratia Farm in Urbana, Illinois, USA.
+//  An example of how to send data via ESP-NOW using FDRS.
+//
+
+#include "fdrs_node_config.h"
+#include <fdrs_node.h>
+
+void fdrs_recv_cb(DataReading theData) {
+  DBG("ID: " + String(theData.id));
+  DBG("Type: " + String(theData.t));
+  DBG("Data: " + String(theData.d));
+}
+
+void setup() {
+  beginFDRS();
+  //pingFDRS(1000);
+  addFDRS(fdrs_recv_cb);
+  subscribeFDRS(READING_ID);
+}
+void loop() {
+loopFDRS();
+}
