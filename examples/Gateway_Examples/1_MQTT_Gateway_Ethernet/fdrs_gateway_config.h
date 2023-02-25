62032a1d2af336dfcddd35f1a3beb56c68f6435f
Timm Bogner <64260873+timmbogner@users.noreply.github.com>
2023-02-25 16:30:42 -0600

    ethernet gateway example
    Co-Authored-By: generalmanager <6797943+generalmanager@users.noreply.github.com>

---
+++ examples/Gateway_Examples/1_MQTT_Gateway_Ethernet/fdrs_gateway_config.h
@@ -0,0 +1,75 @@
+//  FARM DATA RELAY SYSTEM
+//
+//  ETHERNET GATEWAY CONFIGURATION
+
+//Addresses
+#define UNIT_MAC           0x01  // The address of this gateway
+
+#define ESPNOW_NEIGHBOR_1  0x00  // Address of ESP-NOW neighbor #1
+#define ESPNOW_NEIGHBOR_2  0x02  // Address of ESP-NOW neighbor #2
+#define LORA_NEIGHBOR_1    0x00  // Address of LoRa neighbor #1
+#define LORA_NEIGHBOR_2    0x00  // Address of LoRa neighbor #2
+
+// Interfaces
+#define USE_ESPNOW  
+//#define USE_LORA
+//#define USE_WIFI  // Will cause errors if used with ESP-NOW. Use a serial link instead!
+#define USE_ETHERNET
+
+// Actions
+// Options: sendESPNowNbr(1 or 2); sendESPNowPeers(); sendLoRaNbr(1 or 2); broadcastLoRa(); sendSerial(); sendMQTT();
+#define ESPNOWG_ACT    sendMQTT();
+#define LORAG_ACT      
+#define SERIAL_ACT     sendESPNowNbr(2); sendESPNowPeers();
+#define MQTT_ACT          
+#define INTERNAL_ACT   sendMQTT();
+#define ESPNOW1_ACT    
+#define ESPNOW2_ACT    sendMQTT();                
+#define LORA1_ACT      
+#define LORA2_ACT      
+
+// LoRa Configuration
+#define RADIOLIB_MODULE SX1276
+#define LORA_SS    18
+#define LORA_RST   14
+#define LORA_DIO   26
+#define LORA_TXPWR 17   // LoRa TX power in dBm (: +2dBm - +17dBm (for SX1276-7) +20dBm (for SX1278))
+//#define USE_SX126X
+
+//#define CUSTOM_SPI
+#define LORA_SPI_SCK  5
+#define LORA_SPI_MISO 19
+#define LORA_SPI_MOSI 27
+
+#define FDRS_DEBUG     // Enable USB-Serial debugging
+
+// OLED -- Displays console debugging messages on an SSD1306 I²C OLED
+///#define USE_OLED    
+#define OLED_HEADER "FDRS"
+#define OLED_SDA 4
+#define OLED_SCL 15
+#define OLED_RST 16
+
+// UART data interface pins (if available)
+#define RXD2 14
+#define TXD2 15
+
+//#define USE_LR  // Use ESP-NOW LR mode (ESP32 only)
+
+// WiFi and MQTT Credentials  -- These will override the global settings
+//#define WIFI_SSID   "Your SSID"  
+//#define WIFI_PASS   "Your Password"
+
+#define MQTT_ADDR   "192.168.2.3"
+//#define MQTT_PORT   1883 // Default MQTT port is 1883
+//#define MQTT_AUTH   //Enable MQTT authentication 
+//#define MQTT_USER   "Your MQTT Username"
+//#define MQTT_PASS   "Your MQTT Password"
+
+// Logging settings  --  Logging will occur when MQTT is disconnected
+//#define USE_SD_LOG        //Enable SD-card logging
+//#define USE_FS_LOG        //Enable filesystem (flash) logging
+#define LOGBUF_DELAY 10000  // Log Buffer Delay - in milliseconds
+#define SD_SS        0      //SD card CS pin (Use different pins for LoRa and SD)
+#define LOG_FILENAME "fdrs_log.csv"
+
